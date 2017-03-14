c9cbc8d5af4e96796fde0b09dae02da04292b503
HimanshuP90 <himanshup895@gmail.com>
2017-03-14 17:48:18 +0530

    Signup and Sign in Static

--- App.js
+++ App.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import ContactManager from './ContactManager';
 
 class App extends React.Component {
    render() {
       return (
          <div>
+         <ContactManager />
          </div>
       );
    }
