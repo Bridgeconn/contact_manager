ff1ec8fe8b1da738e845d9b4d2c74f11624e1c0e
HimanshuP90 <himanshup895@gmail.com>
2017-03-15 13:05:32 +0530

    .gitignore file

--- schema.sql
+++ schema.sql
@@ -4,3 +4,11 @@
   title  VARCHAR(100) NOT NULL,
   body   TEXT         NOT NULL
 );
+
+CREATE TABLE IF NOT EXISTS `Signup` (
+  `userId` int(11) NOT NULL AUTO_INCREMENT,
+  `userEmail` varchar(60) NOT NULL,
+  `userPass` varchar(255) NOT NULL,
+  PRIMARY KEY (`userId`),
+  UNIQUE KEY `userEmail` (`userEmail`)
+);
