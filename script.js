48398bcc00aef70e6197978b75b586db88acb7c3
udkumar <udkumar@hotmail.com>
2017-03-29 16:24:12 +0530

    add test function code

--- script.js
+++ script.js
@@ -1,31 +1,25 @@
-var mysql      = require('mysql');
-var connection = mysql.createConnection({
-  host     : 'localhost',
-  user     : 'root',
-  password : 'root',
-  database : 'contactmanager'
-});
+// var mysql      = require('mysql');
 
-connection.connect(function(error) {
-    if(!!error) {
-      console.log('Error');
-    } else {
-      console.log('Connected');
-    }
-})
+// var connection = mysql.createConnection({
+//   host     : 'localhost',
+//   user     : 'root',
+//   password : 'root',
+//   database : 'contactmanager'
+// });
 
-var myself = {
-  author: 'Himanshu Panbdey',
-  title: 'User authenicate',
-  body: 'contact_manager_book'
-};
+// connection.connect(function(error) {
+//     if(!!error) {
+//       console.log('Error');
+//     } else {
+//       console.log('Connected');
+//     }
+// })
 
-var query = connection.query('insert into myself set ?', myself, function (err, result) {
-  if (err) {
-    console.error(err);
-    return;
+// connection.end();
+var testing = {
+  t1: function(email, username){
+    
   }
-  console.error(result);
-});
+}
 
-connection.end();
+module.exports = testing;
