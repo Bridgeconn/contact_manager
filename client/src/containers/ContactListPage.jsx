6e5987f00448ac62e916d1d272c1e179978d992b
neetu <neetuy1455@gmail.com>
2017-04-18 17:07:47 +0530

    update master with latest code

---
+++ client/src/containers/ContactListPage.jsx
@@ -0,0 +1,16 @@
+import React, { PropTypes } from 'react';
+import ContactListForm from '../components/ContactListForm.jsx';
+
+
+
+class ContactListPage extends React.Component {
+  render(){
+    return (
+        <ContactListForm
+        />
+      );
+
+}
+}
+
+export default ContactListPage;
