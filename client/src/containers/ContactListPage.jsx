4fada90ca9f3a67069cbe97c43382643e5459a1a
neetu <neetuy1455@gmail.com>
2017-04-19 14:14:39 +0530

    adding search box

--- client/src/containers/ContactListPage.jsx
+++ client/src/containers/ContactListPage.jsx
@@ -4,13 +4,24 @@ import ContactListForm from '../components/ContactListForm.jsx';
 
 
 class ContactListPage extends React.Component {
+	constructor(props) {
+    super(props);
+    this.onUpdateInput = this.onUpdateInput.bind(this);
+    this.state = {
+      dataSource : [],
+      inputValue : ''
+    }
+  }
+
+  onUpdateInput(inputValue) {
+  	}
   render(){
     return (
-        <ContactListForm
-        />
-      );
-
-}
+        <AutoComplete
+            dataSource    = {this.state.dataSource}
+            onUpdateInput = {this.onUpdateInput} />
+        );
+  }
 }
 
 export default ContactListPage;
