044a5be7510487b55a5f4ef7fa724eebe9803948
neetu <neetuy1455@gmail.com>
2017-04-24 14:55:52 +0530

    alignment

--- client/src/components/ContactForm.jsx
+++ client/src/components/ContactForm.jsx
@@ -18,67 +18,67 @@ const ContactForm = ({
 
       {errors.summary && <p className="error-message">{errors.summary}</p>}
 
-     <Row>
-    <Col sm={6}>
-    
-      <div className="field-line">
-        <TextField
-          floatingLabelText="Name"
-          name="name"
-          onChange={onChange}
-          errorText={errors.name}
-          value={user.name}
-        />
-      </div>
-
-       <div className="field-line">
-        <TextField
-          floatingLabelText="Email"
-          name="email"
-          errorText={errors.email}
-          onChange={onChange}
-          value={user.email}
-        />
-      </div>
-      </Col>
-      <Col sm={6}>
-       <div className="field-line">
-        <TextField
-          floatingLabelText="Mobile No."
-          name="mobile_no"
-          onChange={onChange}
-          errorText={errors.mobile_no}
-          value={user.mobile_no}
-        />
-      </div>
-
-       <div className="field-line">
-        <TextField
-          floatingLabelText="Education"
-          name="education"
-          onChange={onChange}
-          errorText={errors.education}
-          value={user.education}
-        />
-      </div>
-      </Col>
-      <Col sm={6}>
-       <div className="field-line">
-        <TextField
-          floatingLabelText="Address"
-          name="address"
-          onChange={onChange}
-          errorText={errors.address}
-          value={user.address}
-        />
-      </div>
-      </Col>
       <Row>
-      <Col className="offSet2">
-      <div className="button-line">
-        <RaisedButton type="submit" label="Add Contact" primary />
-      </div>
-      </Col>
+        <Col sm={6}>
+    
+          <div className="field-line">
+            <TextField
+              floatingLabelText="Name"
+              name="name"
+              onChange={onChange}
+              errorText={errors.name}
+              value={user.name}
+            />
+          </div>
+
+          <div className="field-line">
+            <TextField
+              floatingLabelText="Email"
+              name="email"
+              errorText={errors.email}
+              onChange={onChange}
+              value={user.email}
+            />
+          </div>
+        </Col>
+        <Col sm={6}>
+          <div className="field-line">
+            <TextField
+              floatingLabelText="Mobile No."
+              name="mobile_no"
+              onChange={onChange}
+              errorText={errors.mobile_no}
+              value={user.mobile_no}
+            />
+          </div>
+
+          <div className="field-line">
+            <TextField
+              floatingLabelText="Education"
+              name="education"
+              onChange={onChange}
+              errorText={errors.education}
+              value={user.education}
+            />
+          </div>
+        </Col>
+        <Col sm={6}>
+          <div className="field-line">
+            <TextField
+              floatingLabelText="Address"
+              name="address"
+              onChange={onChange}
+              errorText={errors.address}
+              value={user.address}
+            />
+          </div>
+        </Col>
+      <Row>
+        <Col className="offSet2">
+          <div className="button-line">
+            <RaisedButton type="submit" label="Add Contact" primary />
+          </div>
+        </Col>
       </Row>
       </Row>
     </form>
